db79bf486908551140649220046997c415866858
Roy411 <mefstathiou91@hotmail.com>
2013-11-25 15:38:43 +0200

    Update ReportDetailActivity.java

--- ReportDetailActivity.java
+++ ReportDetailActivity.java
@@ -24,6 +24,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
+
 /**
  * 
  * @author Andreas 
@@ -49,6 +50,8 @@ public class ReportDetailActivity extends FragmentActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.report_detail_container, fragment).commit();
 		}
+		
+
 	}
 
 	@Override
